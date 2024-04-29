997254452ea3429973053af76f59c4f88cb4ae7c
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-04-29 19:10:44 +0600

    Simplify the state logic and add some more way to update the counter function

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,34 @@ const App = () => {
 	]);
 
 	const handleIncrement = (counterId) => {
-		// increment the counter
+		// WAY _ONE
 		// const findCounter = counters.map((counter) =>
 		// 	counter.id === counterId
 		// 		? { ...counter, value: counter.value + 1 }
 		// 		: counter
 		// );
 		// setCounters(findCounter);
+		// WAY _TWO
+		// const newCounters = [...counters];
+		// const findIndex = newCounters.findIndex(
+		// 	(counter) => counter.id === counterId
+		// ); // 0
+		// newCounters[findIndex].value++;
+		// setCounters(newCounters);
+		// WAY _THREE
 
-		const newCounters = [...counters];
-		const findIndex = newCounters.findIndex(
-			(counter) => counter.id === counterId
-		); // 0
-		newCounters[findIndex].value++;
+		const filteredCounter = counters.map((c) => {
+			if (c.id === counterId) {
+				return {
+					...c,
+					value: c.value + 1,
+				};
+			} else {
+				return { ...c };
+			}
+		});
 
-		setCounters(newCounters);
+		setCounters(filteredCounter);
 	};
 	const handleDecrement = (counterId) => {
 		// decrement the counter
@@ -41,26 +54,30 @@ const App = () => {
 		setCounters(findCounter);
 	};
 
-	const totalCount = counters.reduce(
-		(result, currentValue) => result + currentValue.value,
-		0
-	);
+	// const totalCount = counters.reduce(
+	// 	(result, currentValue) => result + currentValue.value,
+	// 	0
+	// );
+
+	const total = () => {
+		return counters.reduce(
+			(result, currentValue) => result + currentValue.value,
+			0
+		);
+	};
 
 	return (
 		<div>
-			<Counter
-				id={1}
-				count={counters[0].value}
-				handleIncrement={handleIncrement}
-				handleDecrement={handleDecrement}
-			/>
-			<Counter
-				id={2}
-				count={counters[1].value}
-				handleIncrement={handleIncrement}
-				handleDecrement={handleDecrement}
-			/>
-			<Stats totalCount={totalCount} />
+			{counters.map((counter) => (
+				<Counter
+					key={counter.id}
+					id={counter.id}
+					count={counter.value}
+					handleIncrement={handleIncrement}
+					handleDecrement={handleDecrement}
+				/>
+			))}
+			<Stats totalCount={total()} />
 		</div>
 	);
 };
