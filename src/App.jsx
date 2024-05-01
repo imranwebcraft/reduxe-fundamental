8c647fb130602ce85e20a05a95926e2a51535e0c
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-05-02 00:21:29 +0600

    learn use of useSelector and useDispatch hook usecase from redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,11 @@
-import { useState } from "react";
 import Counter from "./components/Counter";
-import Stats from "./components/Stats";
+// import Stats from "./components/Stats";
 
 const App = () => {
-	const [counters, setCounters] = useState([
-		{
-			id: 1,
-			value: 0,
-		},
-		{
-			id: 2,
-			value: 0,
-		},
-	]);
-
-	const handleIncrement = (counterId) => {
-		// WAY _ONE
-		// const findCounter = counters.map((counter) =>
-		// 	counter.id === counterId
-		// 		? { ...counter, value: counter.value + 1 }
-		// 		: counter
-		// );
-		// setCounters(findCounter);
-		// WAY _TWO
-		// const newCounters = [...counters];
-		// const findIndex = newCounters.findIndex(
-		// 	(counter) => counter.id === counterId
-		// ); // 0
-		// newCounters[findIndex].value++;
-		// setCounters(newCounters);
-		// WAY _THREE
-
-		const filteredCounter = counters.map((c) => {
-			if (c.id === counterId) {
-				return {
-					...c,
-					value: c.value + 1,
-				};
-			} else {
-				return { ...c };
-			}
-		});
-
-		setCounters(filteredCounter);
-	};
-	const handleDecrement = (counterId) => {
-		// decrement the counter
-		const findCounter = counters.map((counter) =>
-			counter.id === counterId
-				? { ...counter, value: counter.value - 1 }
-				: { ...counter }
-		);
-		setCounters(findCounter);
-	};
-
-	// const totalCount = counters.reduce(
-	// 	(result, currentValue) => result + currentValue.value,
-	// 	0
-	// );
-
-	const total = () => {
-		return counters.reduce(
-			(result, currentValue) => result + currentValue.value,
-			0
-		);
-	};
-
 	return (
 		<div>
-			{counters.map((counter) => (
-				<Counter
-					key={counter.id}
-					id={counter.id}
-					count={counter.value}
-					handleIncrement={handleIncrement}
-					handleDecrement={handleDecrement}
-				/>
-			))}
-			<Stats totalCount={total()} />
+			<Counter />
+			{/* <Stats totalCount={total()} /> */}
 		</div>
 	);
 };
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,22 @@
-const Counter = ({ count, handleDecrement, handleIncrement, id }) => {
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../Redux/features/counter/counterSlice";
+
+const Counter = () => {
+	const value = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+
 	return (
 		<div className="w-full flex flex-col justify-center items-center mt-16 border-b-2">
-			<h1 className=" text-5xl font-bold mb-2">{count}</h1>
+			<h1 className=" text-5xl font-bold mb-2">{value}</h1>
 			<div className=" flex gap-12 pb-5">
 				<button
-					onClick={() => handleIncrement(id)}
+					onClick={() => dispatch(increment())}
 					className="bg-purple-500 px-3 py-2 text-white rounded"
 				>
 					Increment
 				</button>
 				<button
-					onClick={() => handleDecrement(id)}
+					onClick={() => dispatch(decrement())}
 					className="bg-rose-500 px-3 py-2 text-white rounded"
 				>
 					Decrement
